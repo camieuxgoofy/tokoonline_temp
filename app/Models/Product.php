4ed4f9358fb6487fae0a6751573d7cf96978623f
rizkynurmega <rizkynurmega@gmail.com>
2022-11-07 23:32:52 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'parent_id',
+        'user_id',
+        'sku',
+        'type',
+        'name',
+        'slug',
+        'price',
+        'weight',
+        'length',
+        'width',
+        'height',
+        'short_description',
+        'description',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function productInventory()
+	{
+		return $this->hasOne('App\Models\ProductInventory');
+	}
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Models\Category', 'product_categories');
+    }
+
+    public function variants()
+	{
+		return $this->hasMany('App\Models\Product', 'parent_id')->orderBy('price', 'ASC');
+	}
+
+	public function parent()
+	{
+		return $this->belongsTo('App\Models\Product', 'parent_id');
+	}
+
+    public function productAttributeValues()
+	{
+		return $this->hasMany('App\Models\ProductAttributeValue', 'parent_product_id');
+	}
+
+    public function productImages()
+	{
+		return $this->hasMany('App\Models\ProductImage')->orderBy('id', 'DESC');
+	}
+
+    public static function statuses()
+    {
+        return [
+            0 => 'draft',
+            1 => 'active',
+            2 => 'inactive',
+        ];
+    }
+
+    public static function types()
+	{
+		return [
+            'simple' => 'Simple',
+            'configurable' => 'Configurable',
+        ];
+	}
+
+    public function statusLabel()
+	{
+		$statuses = $this->statuses();
+
+		return isset($this->status) ? $statuses[$this->status] : null;
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\AttributeController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(
+	['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth']],
+    function () {
+        Route::get('dashboard',[DashboardController::class, 'index']);
+        Route::resource('categories', CategoryController::class);
+
+        Route::resource('products', ProductController::class);
+        Route::get('products/{productID}/images', [ProductController::class, 'images'])->name('products.images');
+        Route::get('products/{productID}/add-image', [ProductController::class, 'addImage'])->name('products.add_image');
+        Route::post('products/images/{productID}', [ProductController::class, 'uploadImage'])->name('products.upload_image');
+		Route::delete('products/images/{imageID}', [ProductController::class, 'removeImage'])->name('products.remove_image');
+
+        Route::resource('attributes', AttributeController::class);
+        Route::get('attributes/{attributeID}/options', [AttributeController::class, 'options'])->name('attributes.options');
+		Route::get('attributes/{attributeID}/add-option', [AttributeController::class, 'add_option'])->name('attributes.add_option');
+		Route::post('attributes/options/{attributeID}', [AttributeController::class, 'store_option'])->name('attributes.store_option');
+		Route::delete('attributes/options/{optionID}', [AttributeController::class, 'remove_option'])->name('attributes.remove_option');
+		Route::get('attributes/options/{optionID}/edit', [AttributeController::class, 'edit_option'])->name('attributes.edit_option');
+		Route::put('attributes/options/{optionID}', [AttributeController::class, 'update_option'])->name('attributes.update_option');
+
+        Route::resource('roles', RoleController::class);
+		Route::resource('users', UserController::class);
+    }
+);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
